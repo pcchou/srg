d85aa7ff2bdad8ab489d6820e28fcf32090899e4
pcchou <philipchou1009@gmail.com>
2014-09-24 20:55:01 +0800

    Fixed ridiculous number issue

--- bash/srg.sh
+++ bash/srg.sh
@@ -4,15 +4,19 @@
 # License: Apache License
 # Added smart mode by pcchou http://about.me/pcchou
 
+function rand(){
+    echo $(awk 'BEGIN{srand();print int(rand()*(('$2')-('$1')))+('$1') }')
+}
+
 if [ $1 == "normal" ]; then
     pingms=$4
     dlkbps=$2
     ulkbps=$3
 elif [ $1 == "smart" ]; then
-    dlkbps=$(( $2 * ( 950000 + ( RANDOM %= 20000 - 70000 ) ) / 1000000 ))
-    ulkbps=$(( ( $dlkbps * 1000 - ( ( RANDOM %= 500 + 9250 ) ) ) / 1000))
+    dlkbps=$(( $2 * ( 950000 + $(rand -50000 20000) ) / 1000000 ))
+    ulkbps=$(( ( $dlkbps * $(rand 9250 9750) ) / 10000 ))
     if [ $4 -ge 4 ]; then
-        pingms=$(( $4 + ( RANDOM %= 7 + 3 ) ))
+        pingms=$(( $4 + $(rand -3 10)))
     else
         pingms=$4
     fi
@@ -22,7 +26,8 @@ else
 fi
 srv=${5:-2181}
 
-
+echo $dlkbps
+echo $ulkbps
 hash=$(echo -n $pingms-$ulkbps-$dlkbps-297aae72 | md5sum | awk '{print $1}')
 resultid=$(curl -s -d "download=$dlkbps&ping=$pingms&upload=$ulkbps&promo=&startmode=pingselect&recommendedserverid=2181&accuracy=1&serverid=$srv&hash=$hash" -e http://c.speedtest.net/flash/speedtest.swf http://www.speedtest.net/api/api.php | awk -F=\|\& '{print $2}')
 
